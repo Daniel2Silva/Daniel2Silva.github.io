9b965a65c88e9c9afc3481b2537f56681304f39f
Daniel Silva <danicater@hotmail.com>
2023-08-14 00:15:02 -0300

    animao ao scroll

--- script.js
+++ script.js
@@ -28,25 +28,46 @@ function menuMobile() {
     nav.classList.toggle("ativo");
   }
   menumobile.addEventListener("click", ativarMenu);
-};
+}
 
 menuMobile();
 
-const linksInternos = document.querySelectorAll('.menu a[href^="#"]');
+function InitScollSuave() {
+  const linksInternos = document.querySelectorAll('.menu a[href^="#"]');
 
-function scrollSuave(event){
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
+  function scrollSuave(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
 
-  section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'center'
-  })
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollSuave);
+  });
 }
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollSuave);
-});
+InitScollSuave();
 
+const scrollAnimado = document.querySelectorAll(".js-scroll");
+const windowTamanho = window.innerHeight * 0.6;
 
+function animaScroll() {
+  scrollAnimado.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowTamanho;
+    if (sectionTop < 0) {
+      section.classList.add('ativo');
+    }
+    else{
+      section.classList.remove('ativo')
+    }
+  });
+}
+
+animaScroll();
+
+window.addEventListener("scroll", animaScroll);
